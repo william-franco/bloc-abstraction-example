df25db01acc84710b022f031d697d7a97382b0e6
William Franco <williamff@outlook.com>
2025-05-02 16:41:23 -0300

    :building_construction: Update project architecture.

--- android/app/src/main/kotlin/br/com/williamfranco/bloc_abstraction_example/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/williamfranco/bloc_abstraction_example/MainActivity.kt
@@ -2,4 +2,4 @@ package br.com.williamfranco.bloc_abstraction_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
